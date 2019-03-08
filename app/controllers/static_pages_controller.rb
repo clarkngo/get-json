626f3a0be315b21d32bf34c8b60c045eac315853
Clark Ngo <clarkngo@gmail.com>
2019-03-08 15:55:18 -0800

    add json parse

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
+    require 'net/http'
+
+    url = 'https://smashbros-clark-ngo.herokuapp.com/posts.json'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @obj = JSON.parse(response)
   end
+
 end
